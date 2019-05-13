70ba96487a9dd906537af1c0a411759e16576d3d
Pär Hansson <par.hansson@gmail.com>
2019-05-13 23:06:13 +0200

    Update webpack and other dependencies

--- ng2/config/webpack.prod.js
+++ ng2/config/webpack.prod.js
@@ -3,7 +3,8 @@ const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const commonConfig = require('./webpack.common.js');
 const helpers = require('./helpers');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
 const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
@@ -24,16 +25,19 @@ module.exports = webpackMerge(commonConfig, {
     noEmitOnErrors: true, // NoEmitOnErrorsPlugin
     minimizer: [
       // Added to makte opentype work when miminized. bug in uglify, reuses variables names in inlined functions
-      new UglifyJSPlugin({
+      new TerserPlugin({
           parallel: true,  // Webpack default
           cache: true,      // Webpack default
-          uglifyOptions: {
+          terserOptions:{
+            compress: true //{ inline: false },
+          },
+          //uglifyOptions: {
               /*
                   inlining is broken sometimes where inlined function uses the same variable name as inlining function.
                   See https://github.com/mishoo/UglifyJS2/issues/2842, https://github.com/mishoo/UglifyJS2/issues/2843
                */
-              compress: { inline: false },
-          },
+          //    compress: { inline: false },
+          //},
       })
   ],    
   },
